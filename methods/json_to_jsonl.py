4e1be45c9785029e07007deefd3dd11a36d6ae24
muzi <rkwork17527796274@gmail.com>
2024-06-26 17:08:45 +0800

    第五次提交：新增了鸣春谷游览区数据至700条

--- methods/json_to_jsonl.py
+++ methods/json_to_jsonl.py
@@ -1,9 +1,9 @@
 import json
 
-with open ('/home/rkwork/work_place/project/rk_datasets/tempd_eval.json', mode='r') as f:
+with open ('/home/rkwork/work_place/project/rk_datasets/temp.json', mode='r') as f:
     data = json.load(f)
     print(len(data))
 
-with open('./daiding.jsonl', mode='a+', encoding='utf-8') as f :
+with open('./temp.jsonl', mode='a+', encoding='utf-8') as f :
     for dataset in data:
         f.write(json.dumps(dataset, ensure_ascii =False) + '\n')
--- methods/random_eval.py
+++ methods/random_eval.py
@@ -4,18 +4,18 @@ import json
 
 start = 0
 end = 50
-with open('/home/rkwork/work_place/project/rk_datasets/temp.jsonl', 'r') as f:     
+with open('temp.jsonl', 'r') as f:     
 
     # data = json.load(f)             # 处理json格式
     data = [json.loads(line) for line in f]
 
 with open('temp_eval.jsonl', 'a+', encoding='utf-8') as f:
 
-    for number in range(8):
+    for number in range(14):
         for item in range(10):
             # query = item['instruction']
             random_number = random.randint(start, end)
-            print(f"生成的随机数是: {random_number}")
+            # print(f"生成的随机数是: {random_number}")
             f.write(json.dumps(data[random_number], ensure_ascii= False ) + '\n')
         
         start += 50
@@ -23,3 +23,4 @@ with open('temp_eval.jsonl', 'a+', encoding='utf-8') as f:
         
 
 
+print(end)
