2beecf279ee026c661c338a83d45981c60bd5391
muzi <rkwork17527796274@gmail.com>
2024-06-26 15:06:54 +0800

    rk: 第四题提交：关于三台岭和麓湖两个游览区数据集共200条

--- methods/json_to_jsonl.py
+++ methods/json_to_jsonl.py
@@ -1,7 +1,8 @@
 import json
 
-with open ('/home/rkwork/work_place/project/rk_datasets/rk_experiment/3_gz_baiyunshan/daiding.json', mode='r') as f:
+with open ('/home/rkwork/work_place/project/rk_datasets/tempd_eval.json', mode='r') as f:
     data = json.load(f)
+    print(len(data))
 
 with open('./daiding.jsonl', mode='a+', encoding='utf-8') as f :
     for dataset in data:
